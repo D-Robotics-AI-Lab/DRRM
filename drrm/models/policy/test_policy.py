6393e3a9d32a799d476b985983447b52e3d79ad3
fa.fu <alanmathisonturing@163.com>
2025-05-27 08:44:38 +0000

    modify TestPolicy

--- drrm/models/policy/test_policy.py
+++ drrm/models/policy/test_policy.py
@@ -10,7 +10,9 @@ class TestPolicy(BasePolicy):
         self.model = torchvision.models.resnet18(weights=torchvision.models.ResNet18_Weights.IMAGENET1K_V1)
         self.model.fc = torch.nn.Linear(self.model.fc.in_features, num_classes)
 
-    def compute_loss(self, images, labels):
+    def compute_loss(self, batch):
+        images = batch[0]
+        labels = batch[1]
         preds = self.model(images)  # (batch_size, num_classes)
         labels = labels.long()
         loss = torch.nn.functional.cross_entropy(preds, labels)  # (batch_size,)
