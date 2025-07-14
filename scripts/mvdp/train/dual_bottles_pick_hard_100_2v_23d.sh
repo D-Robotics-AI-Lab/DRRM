194af3a37c09cdae372e2bebc893b2f612a8cfc0
NZH <cheney23333@gamil.com>
2025-07-14 21:28:36 +0800

    Default: seq_as_view=False Changes to be committed: 	modified:   configs/mvdp_train/mvdp_1v_23d.yaml 	modified:   configs/mvdp_train/mvdp_2v_23d.yaml 	modified:   configs/mvdp_train/mvdp_2v_23d_23.yaml 	modified:   configs/mvdp_train/mvdp_2v_3d.yaml 	renamed:    scripts/mvdp/train/dual_bottles_pick_hard_100_1v_23d_sav.sh -> scripts/mvdp/train/dual_bottles_pick_hard_100_1v_23d.sh 	new file:   scripts/mvdp/train/dual_bottles_pick_hard_100_2v_23d.sh 	renamed:    scripts/mvdp/train/dual_bottles_pick_hard_100_2v_23d_sav_23.sh -> scripts/mvdp/train/dual_bottles_pick_hard_100_2v_23d_23.sh 	modified:   scripts/mvdp/train/dual_bottles_pick_hard_100_2v_23d_sav.sh 	renamed:    scripts/mvdp/train/dual_bottles_pick_hard_100_2v_3d_sav.sh -> scripts/mvdp/train/dual_bottles_pick_hard_100_2v_3d.sh

--- scripts/mvdp/train/dual_bottles_pick_hard_100_2v_23d.sh
+++ scripts/mvdp/train/dual_bottles_pick_hard_100_2v_23d.sh
@@ -1,14 +1,13 @@
 config=mvdp_2v_23d
 task=dual_bottles_pick_hard
 demo=100
-path="${config/_*/}_${task}_${demo}_${config#*_}_sav"
+path="${config/_*/}_${task}_${demo}_${config#*_}"
 
 accelerate launch --num_processes 8\
     --config_file configs/accelerate_config.yaml \
     main.py \
     --config-path=configs/mvdp_train \
     --config-name=$config.yaml \
-    model.obs_encoder.emvis_config.seq_as_view=True \
     train_dataset.task=$task \
     train_dataset.demo=$demo
 
