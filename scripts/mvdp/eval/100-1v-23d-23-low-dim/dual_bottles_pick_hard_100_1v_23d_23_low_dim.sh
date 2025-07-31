0af3c351b039a212b25c9b0a84d4b94589d1a80c
NZH <cheney23333@gmail.com>
2025-07-31 13:58:52 +0800

    Update: 100-1v-23d-23-low-dim eval scripts
    Changes to be committed:
    	new file:   scripts/mvdp/eval/100-1v-23d-23-low-dim/block_hammer_beat_100_1v_23d_23_low_dim.sh
    	new file:   scripts/mvdp/eval/100-1v-23d-23-low-dim/bottle_adjust_100_1v_23d_23_low_dim.sh
    	new file:   scripts/mvdp/eval/100-1v-23d-23-low-dim/container_place_100_1v_23d_23_low_dim.sh
    	new file:   scripts/mvdp/eval/100-1v-23d-23-low-dim/dual_bottles_pick_hard_100_1v_23d_23_low_dim.sh
    	new file:   scripts/mvdp/eval/100-1v-23d-23-low-dim/put_apple_cabinet_100_1v_23d_23_low_dim.sh

---
+++ scripts/mvdp/eval/100-1v-23d-23-low-dim/dual_bottles_pick_hard_100_1v_23d_23_low_dim.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+config=mvdp_1v_23d_23_low_dim
+task=dual_bottles_pick_hard
+demo=100
+path="${config/_*/}_${task}_${demo}_${config#*_}"
+
+./scripts/mvdp/eval/evaluation.sh $config $task $demo $path
