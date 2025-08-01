9f6bdca90487c3b32bdb37fdcdf487140f93370c
NZH <cheney23333@gmail.com>
2025-08-01 16:57:34 +0800

    Update: mvdp-1v-3d-23-frame train scripts
    Changes to be committed:
    	new file:   configs/mvdp_train/mvdp_1v_3d_23_frame.yaml
    	new file:   scripts/mvdp/train/100-1v-3d-23-frame/block_hammer_beat_100_1v_3d_23_frame.sh
    	new file:   scripts/mvdp/train/100-1v-3d-23-frame/bottle_adjust_100_1v_3d_23_frame.sh
    	new file:   scripts/mvdp/train/100-1v-3d-23-frame/container_place_100_1v_3d_23_frame.sh
    	new file:   scripts/mvdp/train/100-1v-3d-23-frame/dual_bottles_pick_hard_100_1v_3d_23_frame.sh
    	new file:   scripts/mvdp/train/100-1v-3d-23-frame/put_apple_cabinet_100_1v_3d_23_frame.sh

---
+++ scripts/mvdp/train/100-1v-3d-23-frame/put_apple_cabinet_100_1v_3d_23_frame.sh
@@ -0,0 +1,14 @@
+config=mvdp_1v_3d_23_frame
+task=put_apple_cabinet
+demo=100
+path="${config/_*/}_${task}_${demo}_${config#*_}"
+
+accelerate launch\
+    --config_file configs/accelerate_config.yaml \
+    main.py \
+    --config-path=configs/mvdp_train \
+    --config-name=$config.yaml \
+    train_dataset.task=$task \
+    train_dataset.demo=$demo
+
+~/bcecmd bos cp -r checkpoints/$path bos:/dg-algo/zehao.ni/checkpoints/$path
