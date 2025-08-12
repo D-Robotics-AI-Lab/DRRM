9ef86a2233a1ac3a5923607742b06b6a1080c65d
NZH <cheney23333@gmail.com>
2025-08-12 21:28:46 +0800

    Update: null-1v-23d-23-global train scripts
    Changes to be committed:
    	modified:   configs/mvdp_train/mvdp4thanos_1v_23d_23_global.yaml
    	renamed:    scripts/mvdp/train/null-1v-23d-23-global/Place the blue square object into the bowl.sh -> scripts/mvdp/train/null-1v-23d-23-global/place_blue.sh
    	renamed:    scripts/mvdp/train/null-1v-23d-23-global/Place the green rectangular object into the bowl.sh -> scripts/mvdp/train/null-1v-23d-23-global/place_green.sh

--- scripts/mvdp/train/null-1v-23d-23-global/place_blue.sh
+++ scripts/mvdp/train/null-1v-23d-23-global/place_blue.sh
@@ -1,5 +1,5 @@
 config=mvdp4thanos_1v_23d_23_global
-task='Place the blue square object into the bowl'
+task=place_blue
 demo=None
 path="${config/_*/}_${task}_${demo}_${config#*_}"
 
@@ -8,7 +8,7 @@ accelerate launch\
     main.py \
     --config-path=configs/mvdp_train \
     --config-name=$config.yaml \
-    train_dataset.task="$task" \
+    train_dataset.task=$task \
     train_dataset.demo=$demo
 
 ~/bcecmd bos cp -r "checkpoints/$path" "bos:/dg-algo/zehao.ni/checkpoints/$path"
