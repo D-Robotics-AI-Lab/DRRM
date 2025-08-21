bbc442cc61e0665d750e75ee6b11450ac4397c07
NZH <cheney23333@gmail.com>
2025-08-21 18:06:20 +0800

    Fix: main_process_port bug
    Changes to be committed:
    	modified:   configs/mvdp_pretrain/dp_baseline.yaml
    	renamed:    scripts/mvdp/pretrain/100_1v_3d_23_pool.sh -> scripts/mvdp/pretrain/100_1v_23d_23_pool.sh

--- scripts/mvdp/pretrain/100_1v_23d_23_pool.sh
+++ scripts/mvdp/pretrain/100_1v_23d_23_pool.sh
@@ -1,11 +1,12 @@
-config=mvdp_1v_3d_23_pool
+config=mvdp_1v_23d_23_pool
 path="${config/_*/}_pretrain_${config#*_}"
-
+export NCCL_DEBUG=INFO              
+export NCCL_ASYNC_ERROR_HANDLING=1 
 accelerate launch\
     --config_file configs/accelerate_config_mm.yaml \
     --machine_rank ${DEEPSEED_RANK} \
     --main_process_ip 192.168.80.7 \
-    --main_process_ip 10086 \
+    --main_process_port 10086 \
     main.py \
     --config-path=configs/mvdp_pretrain \
     --config-name=$config.yaml \
