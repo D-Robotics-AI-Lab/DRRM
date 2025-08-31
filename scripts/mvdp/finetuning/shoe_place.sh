35c66942f7b8513c7f89cbb5d5eef4df1932dea6
NZH <cheney23333@gmail.com>
2025-09-01 02:58:37 +0800

    Update: mvdp_finetuning train scripts
    Changes to be committed:
    	new file:   configs/mvdp_finetuning/dp_baseline.yaml
    	new file:   configs/mvdp_finetuning/mvdp_1v_23d_23_pool.yaml
    	new file:   scripts/mvdp/finetuning/block_hammer_beat.sh
    	new file:   scripts/mvdp/finetuning/block_handover.sh
    	new file:   scripts/mvdp/finetuning/blocks_stack_easy.sh
    	new file:   scripts/mvdp/finetuning/bottle_adjust.sh
    	new file:   scripts/mvdp/finetuning/container_place.sh
    	new file:   scripts/mvdp/finetuning/diverse_bottles_pick.sh
    	new file:   scripts/mvdp/finetuning/dual_bottles_pick_easy.sh
    	new file:   scripts/mvdp/finetuning/dual_bottles_pick_hard.sh
    	new file:   scripts/mvdp/finetuning/dual_shoes_place.sh
    	new file:   scripts/mvdp/finetuning/empty_cup_place.sh
    	new file:   scripts/mvdp/finetuning/pick_apple_messy.sh
    	new file:   scripts/mvdp/finetuning/put_apple_cabinet.sh
    	new file:   scripts/mvdp/finetuning/shoe_place.sh
    	new file:   scripts/mvdp/finetuning/tool_adjust.sh

---
+++ scripts/mvdp/finetuning/shoe_place.sh
@@ -0,0 +1,12 @@
+config=mvdp_1v_23d_23_pool
+task=shoe_place
+path="${config/_*/}_${task}_ft_${config#*_}"
+
+accelerate launch\
+    --config_file configs/accelerate_config.yaml \
+    main.py \
+    --config-path=configs/mvdp_finetuning \
+    --config-name=$config.yaml \
+    train_dataset.task=$task
+
+~/bcecmd bos cp -r checkpoints/$path bos:/dg-algo/zehao.ni/checkpoints/$path
