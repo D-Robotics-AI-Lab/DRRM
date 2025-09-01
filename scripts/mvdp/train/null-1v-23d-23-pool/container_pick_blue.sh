5ab8f3495691df6c2022f0d7c9f3c5c3cd60c1c8
NZH <cheney23333@gamil.com>
2025-09-02 02:29:02 +0800

    Changes to be committed: 	modified:   configs/mvdp_train/mvdp4thanos_1v_23d_23_pool.yaml 	modified:   scripts/mvdp/train/null-1v-23d-23-pool/container_pick_blue.sh

--- scripts/mvdp/train/null-1v-23d-23-pool/container_pick_blue.sh
+++ scripts/mvdp/train/null-1v-23d-23-pool/container_pick_blue.sh
@@ -8,6 +8,7 @@ accelerate launch\
     main.py \
     --config-path=configs/mvdp_train \
     --config-name=$config.yaml \
-    train_dataset.task=$task
+    train_dataset.task=$task \
+    train_dataset.demo=$demo
 
 ~/bcecmd bos cp -r "checkpoints/$path" "bos:/dg-algo/zehao.ni/checkpoints/$path"
