7a9b2bc11ecd81abc8caf2b06795141be2a2f1e8
NZH <cheney23333@gamil.com>
2025-09-07 23:01:02 +0800

    Changes to be committed: 	new file:   scripts/dp/train/dp4thanos/place_cube_4p_100.sh 	new file:   scripts/mvdp/train/null-1v-23d-23-pool/place_big_cube_200.sh 	new file:   scripts/mvdp/train/null-1v-23d-23-pool/place_cube_4p_100.sh 	new file:   scripts/mvdp/train/null-1v-23d-23-pool/place_cup_200.sh 	new file:   scripts/mvdp/train/null-1v-23d-23-pool/place_tiny_cube_200.sh

---
+++ scripts/mvdp/train/null-1v-23d-23-pool/place_cube_4p_100.sh
@@ -0,0 +1,14 @@
+config=mvdp4thanos_1v_23d_23_pool
+task=place_cube_4p_100
+demo=null
+path="${config/_*/}_${task}_None_${config#*_}"
+
+accelerate launch\
+    --config_file configs/accelerate_config.yaml \
+    main.py \
+    --config-path=configs/mvdp_train \
+    --config-name=$config.yaml \
+    train_dataset.task=$task \
+    train_dataset.demo=$demo
+
+~/bcecmd bos cp -r "checkpoints/$path" "bos:/dg-algo/zehao.ni/checkpoints/$path"
