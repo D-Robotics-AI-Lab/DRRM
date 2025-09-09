dfdfd0f774a5ee5d668f588ad233597c0b00a3a3
NZH <cheney23333@gamil.com>
2025-09-09 12:40:05 +0800

    Update: 100-1v-23d-23-pool-single-frame train         scripts
    Changes to be committed:
    	new file:   configs/mvdp_train/mvdp_1v_23d_23_pool_single_frame.yaml
    	new file:   scripts/mvdp/train/100-1v-23d-23-pool-single-frame/block_hammer_beat.sh
    	new file:   scripts/mvdp/train/100-1v-23d-23-pool-single-frame/block_handover.sh
    	new file:   scripts/mvdp/train/100-1v-23d-23-pool-single-frame/blocks_stack_easy.sh
    	new file:   scripts/mvdp/train/100-1v-23d-23-pool-single-frame/bottle_adjust.sh
    	new file:   scripts/mvdp/train/100-1v-23d-23-pool-single-frame/container_place.sh
    	new file:   scripts/mvdp/train/100-1v-23d-23-pool-single-frame/diverse_bottles_pick.sh
    	new file:   scripts/mvdp/train/100-1v-23d-23-pool-single-frame/dual_bottles_pick_easy.sh
    	new file:   scripts/mvdp/train/100-1v-23d-23-pool-single-frame/dual_bottles_pick_hard.sh
    	new file:   scripts/mvdp/train/100-1v-23d-23-pool-single-frame/dual_shoes_place.sh
    	new file:   scripts/mvdp/train/100-1v-23d-23-pool-single-frame/empty_cup_place.sh
    	new file:   scripts/mvdp/train/100-1v-23d-23-pool-single-frame/pick_apple_messy.sh
    	new file:   scripts/mvdp/train/100-1v-23d-23-pool-single-frame/put_apple_cabinet.sh
    	new file:   scripts/mvdp/train/100-1v-23d-23-pool-single-frame/shoe_place.sh
    	new file:   scripts/mvdp/train/100-1v-23d-23-pool-single-frame/tool_adjust.sh

---
+++ scripts/mvdp/train/100-1v-23d-23-pool-single-frame/container_place.sh
@@ -0,0 +1,14 @@
+config=mvdp_1v_23d_23_pool_single_frame
+task=container_place
+demo=100
+path="${config/_*/}_${task}_${demo}_${config#*_}"
+
+accelerate launch\
+    --config_file configs/accelerate_config.yaml \
+    main.py \
+    --config-path=configs/mvdp_train \
+    --config-name=$config.yaml \
+    train_dataset.task=$task \
+    train_dataset.demo=$demo
+
+~/bcecmd bos cp -r checkpoints/$path bos:/dg-algo/zehao.ni/checkpoints/$path
