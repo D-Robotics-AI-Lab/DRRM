d219adac7cc0e3cc912ca4c5b7083c5dc01a23cf
NZH <cheney23333@qq.com>
2025-10-12 16:46:20 +0000

    Refactor the code.

--- scripts/exp_sim/dp/eval/baseline/bottle_adjust_100_baseline.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-config=dp_baseline
-task=bottle_adjust
-demo=100
-path="${config/_*/}_${task}_${demo}_${config#*_}"
-
-./scripts/mvdp/eval/evaluation.sh $config $task $demo $path
--- scripts/train_demo.sh
+++ scripts/train_demo.sh
@@ -2,12 +2,13 @@
 dataset=PATH/TO/YOUR/DATASET
 task=YOUR/TASK/NAME
 demo=null # The number of demonstrations used, defaults to all.
+config_dir=configs/vodp/vodp_23d_1frame.yaml
 
 accelerate launch\
     --config_file configs/accelerate_config.yaml \
     main.py \
-    --config-path=configs/vodp \
-    --config-name=vodp_23d_1frame.yaml \
+    --config-path="${config_dir%/*}" \
+    --config-name="${config_dir##*/}" \
     train_dataset.path=$dataset \
     train_dataset.task=$task \
     train_dataset.demo=$demo
